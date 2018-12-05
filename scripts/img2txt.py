f0ecf2620862d0111cb8beb13e19b33ffba5ae40
Dionysios Diamantopoulos <did@zurich.ibm.com>
2018-12-05 19:47:05 +0100

    Fixed scripts/img2txt.py.

--- scripts/img2txt.py
+++ scripts/img2txt.py
@@ -9,14 +9,11 @@ MAX_CLIP = 3.75
 img = Image.open('/tools/projects/LSTM-PYNQ/notebooks/Fraktur_images/010001.raw.lnrm.png').convert('L')
 input_data = np.array(img.getdata())
 
-fraktur_mean = np.mean(input_data)
-fraktur_std_deviation = np.std(input_data)
-
 input_data = input_data * 1.0 / np.amax(input_data)
 input_data = np.amax(input_data) - input_data
 input_data = input_data.T
-
-
+fraktur_mean = np.mean(input_data)
+fraktur_std_deviation = np.std(input_data)
 #w = input_data.shape[0]
 #input_data = np.vstack([np.zeros((PADDING, w)),input_data,np.zeros((PADDING, w))])
 input_data = (input_data - fraktur_mean) / fraktur_std_deviation
